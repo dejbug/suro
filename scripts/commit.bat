9672518c5ab4475bb7c4caff24f16163e75c69dd
Dejan Budimir <dejbug@users.noreply.github.com>
2020-04-06 11:33:17 +0200

    update script can be called from local script

--- scripts/commit.bat
+++ scripts/commit.bat
@@ -1,30 +1,40 @@
 @ECHO OFF
 
 IF "%1" == "" (
-	SET ROOT=%~dp0
+	SET PROJECT_DIR=%~dp0
 ) ELSE (
-	SET ROOT=%~f1\
+	SET PROJECT_DIR=%~f1\
 )
 
-SET SURO_C=%ROOT%suro-c.exe
-SET SURO_W=%ROOT%suro-w.exe
+IF "%2" == "" (
+	SET HOT_DIR=%~dp0
+) ELSE (
+	SET HOT_DIR=%~f2\
+)
 
-FOR %%i IN (%~dp0*.path) DO (
+SET SURO_C=%PROJECT_DIR%suro-c.exe
+SET SURO_W=%PROJECT_DIR%suro-w.exe
+
+FOR %%i IN (%HOT_DIR%*.path) DO (
 	IF NOT EXIST %SURO_W% (
 		ECHO ! "%SURO_W%" not found. Needed for "%%~dpni.exe".
 	) ELSE (
 		ECHO * Copying "%SURO_W%" to "%%~dpni.exe".
-		COPY /B /V "%SURO_W%" "%%~dpni.exe" 1>NUL 2>NUL || ( ECHO ! FAILED. && GOTO :EOF )
+		ECHO COPY /B /V "%SURO_W%" "%%~dpni.exe" 1>NUL 2>NUL || ( ECHO ! FAILED. && GOTO :EOF )
 	)
 )
 
-FOR %%i IN (%~dp0*.bat) DO (
-	IF NOT "%%~fi" == "%~f0" (
-		IF NOT EXIST %SURO_C% (
-			ECHO ! "%SURO_C%" not found. Needed for "%%~dpni.exe".
-		) ELSE (
-			ECHO * Copying "%SURO_C%" to "%%~dpni.exe".
-			COPY /B /V "%SURO_C%" "%%~dpni.exe" 1>NUL 2>NUL || ( ECHO ! FAILED. && GOTO :EOF )
+FOR %%i IN (%HOT_DIR%*.bat) DO (
+	IF "%%~ni" == "%~n0" (
+		ECHO * Skipped "%%~dpni.exe".
+	) ELSE (
+		IF NOT "%%~fi" == "%~f0" (
+			IF NOT EXIST %SURO_C% (
+				ECHO ! "%SURO_C%" not found. Needed for "%%~dpni.exe".
+			) ELSE (
+				ECHO * Copying "%SURO_C%" to "%%~dpni.exe".
+				ECHO COPY /B /V "%SURO_C%" "%%~dpni.exe" 1>NUL 2>NUL || ( ECHO ! FAILED. && GOTO :EOF )
+			)
 		)
 	)
 )
